67d9837731072eeea0bad51f8de9ebab8a89e2d5
eggerino <eggensberger.armin@googlemail.com>
2024-02-25 11:37:57 +0100

    added formatter + solved problem 4

--- src/lists.fsx
+++ src/lists.fsx
@@ -1,23 +1,32 @@
 module lists
 
-    // Problem 1
-    let rec last items =
-        match items with
-        | [] -> None
-        | [item] -> Some(item)
-        | _ :: rest -> last rest
+// Problem 1
+let rec last items =
+    match items with
+    | [] -> None
+    | [ item ] -> Some(item)
+    | _ :: rest -> last rest
 
-    // Problem 2
-    let rec lastTwo items =
-        match items with
-        | [] -> None
-        | [_] -> None
-        | [first; second] -> Some(first, second)
-        | _ :: rest -> lastTwo rest
+// Problem 2
+let rec lastTwo items =
+    match items with
+    | [] -> None
+    | [ _ ] -> None
+    | [ first; second ] -> Some(first, second)
+    | _ :: rest -> lastTwo rest
 
-    // Problem 3
-    let rec at index items =
-        match index, items with
-        | _, [] -> None
-        | 1, item :: _ -> Some(item)
-        | _, _ :: rest -> at (index - 1) rest
+// Problem 3
+let rec at index items =
+    match index, items with
+    | _, [] -> None
+    | 1, item :: _ -> Some(item)
+    | _, _ :: rest -> at (index - 1) rest
+
+// Problem 4
+let length items =
+    let rec recLength current items =
+        match items with
+        | [] -> current
+        | _ :: rest -> recLength (current + 1) rest
+
+    recLength 0 items
